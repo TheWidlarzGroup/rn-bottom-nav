0f177169530ead708b32c6f5a62e2434d2ae3336
mateki0 <mateki0@interia.pl>
2021-06-01 20:11:50 +0200

    nav starter push

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,18 +1,16 @@
-import React from 'react';
-import {SafeAreaProvider} from 'react-native-safe-area-context';
-import {ThemeProvider} from '@shopify/restyle';
+import React from "react";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { ThemeProvider } from "@shopify/restyle";
 
-import {darkTheme, theme} from './utils/theme';
-import {AppNavigation} from './navigation';
+import { darkTheme, theme } from "./utils/theme";
 
 export const Main = () => {
+  // FIXME: read from user preferences
   const darkMode = false;
 
   return (
     <ThemeProvider theme={darkMode ? darkTheme : theme}>
-      <SafeAreaProvider>
-        <AppNavigation />
-      </SafeAreaProvider>
+      <SafeAreaProvider></SafeAreaProvider>
     </ThemeProvider>
   );
 };
